19a08f6161dd470935078ca50c60520068577cd0
Philipp Fruck <dev@p-fruck.de>
2026-05-26 22:15:58 +0200

    chore: add tools.go

---
+++ tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+	_ "github.com/vektra/mockery/v2"
+)
